a88b2d97eed3e8bfdb187d9c3ab758008cf015e2
Jamshid Bahadori <Jamshidbahadoriasd@gmail.com>
2023-04-27 17:56:12 +0200

    adding components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Product from "./components/pages/home/Products";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import ProductForm from "./components/pages/productForm/AddProduct";
 
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Navbar/>
+      <Product/>
+      <Footer/>
+      <ProductForm/>
     </div>
   );
 }
