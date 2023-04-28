fece0c0955597e46b4e006ef3789301d9b974652
Jamshid Bahadori <Jamshidbahadoriasd@gmail.com>
2023-04-28 17:12:20 +0200

    adding cards in productList

--- server.js/server.js
+++ server.js/server.js
@@ -1,9 +1,10 @@
 const express = require("express")
+const cors = require("cors")
 const connection = require("./connection/connection")
 const app = express()
 const router = require("./router/router")
 app.use(express.json())
-
+app.use(cors())
 app.use("/",router)
 const port = 3000;
 app.listen(port,(req,res)=>{console.log(`listening to the port ${port}`)})
